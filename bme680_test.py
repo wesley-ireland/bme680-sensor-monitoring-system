2d62ec5597dcfac8b7dd3ea0a8323e492f813aa9
Wesley Ireland <wesley.j.ireland@gmail.com>
2025-01-02 21:01:30 -0400

    Adding function to read from sensor at address 0x76

--- bme680_test.py
+++ bme680_test.py
@@ -5,12 +5,6 @@ import adafruit_bme680
 import random
 import sys
 
-# Set up I2C communication
-i2c = busio.I2C(board.SCL, board.SDA)
-# sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
-
-user_input = input("To simulate, enter S, for real sensor data, enter R: ")
-
 def simulate_bme680():
     while True:
         temperature = round(random.uniform(20, 35), 2)
@@ -26,9 +20,28 @@ def simulate_bme680():
 
         time.sleep(2)
 
+def read_bme680():
+    # Set up I2C communication
+    i2c = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c, address=0x76)
+
+    while True:
+        # Print temperature, humidity, pressure, and gas resistance
+        print(f"Temperature: {sensor.temperature:.2f} C")
+        print(f"Humidity: {sensor.humidity:.2f} %")
+        print(f"Pressure: {sensor.pressure:.2f} hPa")
+        print(f"Gas Resistance: {sensor.gas:.2f} ohms")
+        print("-------------------------------------")
+
+        time.sleep(2)
+
+
+user_input = input("To simulate, enter S, for real sensor data, enter R: ")
+
 if user_input == "R":
-    print("That doesnt work at the moment.")
-    sys.exit()
+    print("Reading the BME680 sensor data...")
+    print("---------------------------------")
+    read_bme680()
 elif user_input == "S":
     print("Simulating the BME680 sensor data...")
     print("---------------------------------")
@@ -36,16 +49,3 @@ elif user_input == "S":
 else:
     print("That is an invalid command.")
     sys.exit()
-
-# Read data from the BME680
-# while True:
-    # Print temperature, humidity, pressure, and gas resistance
-    #print(f"Temperature: {sensor.temperature:.2f} C")
-    #print(f"Humidity: {sensor.humidity:.2f} %")
-    #print(f"Pressure: {sensor.pressure:.2f} hPa")
-    #print(f"Gas Resistance: {sensor.gas:.2f} ohms")
-    #print("-------------------------------------")
-
- #   print(user_input)
-    # Delay between readings
- #   time.sleep(2)
